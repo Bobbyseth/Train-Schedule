d7443a1e5b9f82223a92eaad46368265b9a225a5
Bobbyseth <seth.griggs007@gmail.com>
2018-02-04 18:26:26 -0600

    What I have so far

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,102 @@
+/* global firebase moment */
+// Steps to complete:
 
-$("#submit").on("click", function(event) {
+// 1. Initialize Firebase
+// 2. Create button for adding new employees - then update the html + update the database
+// 3. Create a way to retrieve employees from the employee database.
+// 4. Create a way to calculate the months worked. Using difference between start and current time.
+//    Then use moment.js formatting to set difference in months.
+// 5. Calculate Total billed
+
+// 1. Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD4Ck8d3-Ch-wWcX9-0t9TIqP6YlD0--bM",
+    authDomain: "train-times-5ac9f.firebaseapp.com",
+    databaseURL: "https://train-times-5ac9f.firebaseio.com",
+    projectId: "train-times-5ac9f",
+    storageBucket: "train-times-5ac9f.appspot.com",
+    messagingSenderId: "394515220595"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// 2. Button for adding Trains
+$("#add-train-btn").on("click", function(event) {
   event.preventDefault();
-  var trainName = $("#trainN").val().trim();
-  var destination = $("#dest").val().trim();
-  var firstTrainTime = $("#firstTT").val().trim();
-  var frequency = $("#freq").val().trim();
-  console.log(trainName);
-  console.log(destination);
-  console.log(firstTrainTime);
-  console.log(frequency);
+
+  // Grabs user input
+  var trainName = $("#train-name-input").val().trim();
+  var trainDestination = $("#destination-input").val().trim();
+  var trainStart = moment($("#start-input").val().trim(), "HH:MM").format("X");
+  var trainFrequency = $("#frequency-input").val().trim();
+
+  // Creates local "temporary" object for holding train data
+  var newTrain = {
+    trainName: trainName,
+    trainDestination: trainDestination,
+    trainStart: trainStart,
+    trainFrequency: trainFrequency
+  };
+
+  // Uploads employee data to the database
+  database.ref().push(newTrain);
+
+  // Logs everything to console
+  console.log(newTrain.trainName);
+  console.log(newTrain.trainDestination);
+  console.log(newTrain.trainStart);
+  console.log(newTrain.trainfrequency);
+
+  // Alert
+  alert("Train successfully added");
+
+  // Clears all of the text-boxes
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#start-input").val("");
+  $("#frequency-input").val("");
+
 });
 
-database.ref().set {
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
-}
+  console.log(childSnapshot.val());
 
-var inputs = $('form').serializeArray();
-console.log(inputs);
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val().role;
+  var trainStart = childSnapshot.val().start;
+  var trainFrequency = childSnapshot.val().rate;
+
+  // Employee Info
+  console.log(trainName);
+  console.log(trainDestination);
+  console.log(trainStart);
+  console.log(trainFrequency);
+
+  // Prettify the employee start
+  var empStartPretty = moment.unix(Start).format("MM/DD/YY");
+
+  // Calculate the months worked using hardcore math
+  // To calculate the months worked
+  var empMonths = moment().diff(moment.unix(empStart, "X"), "months");
+  console.log(empMonths);
+
+  // Calculate the total billed rate
+  var empBilled = empMonths * empRate;
+  console.log(empBilled);
+
+  // Add each train's data into the table
+  $("#employee-table > tbody").append("<tr><td>" + empName + "</td><td>" + empRole + "</td><td>" +
+  empStartPretty + "</td><td>" + empMonths + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
+});
+
+// Example Time Math
+// -----------------------------------------------------------------------------
+// Assume Employee start date of January 1, 2015
+// Assume current date is March 1, 2016
+
+// We know that this is 15 months.
+// Now we will create code in moment.js to confirm that any attempt we use mets this test case
